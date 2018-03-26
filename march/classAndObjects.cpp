4b0076e81fb47143b7dc698c610e4069aff32767
kanishkcoe <32832273+kanishkcoe@users.noreply.github.com>
2018-03-26 19:28:07 +0530

    candd

--- march/classAndObjects.cpp
+++ march/classAndObjects.cpp
@@ -12,6 +12,42 @@ private:
 
   //public:
 public:
+  //constructors
+  //default constructors
+  Complex()
+  {
+    real = 10;
+    imaginary = 40;
+  }
+
+  //parameterised constructors
+  Complex(int r, int i)
+  {
+    real = r;
+    imaginary = i;
+  }
+
+  //copy constructors
+  Complex(Complex &s)
+  {
+    real = s.imaginary;
+    imaginary = s.real;
+  }
+
+  Complex(Complex &s, Complex &k)
+  {
+    real = s.real + k.real;
+    imaginary = s.imaginary + k.imaginary;
+  }
+
+  //destructor
+  ~Complex()
+  {
+    cout << "---------------------------------" << endl;
+    cout << real << " i" << imaginary << endl;
+    cout << "destructor called saksham" << endl;
+    cout << "---------------------------------" << endl;
+  }
   void set(int r, int i)
   {
     real = r;
@@ -27,12 +63,26 @@ public:
 int main()
 {
   Complex c, d;
+  Complex e, f(45, 100);
+  Complex g(f);
+  Complex h = c;
+  Complex k(f, g);
   c.set(3, 5);
   d.set(5, -56);
 
   c.get();
   cout << endl;
   d.get();
+  cout << endl;
+  e.get();
+  cout << endl;
+  f.get();
+  cout << endl;
+  g.get();
+  cout << endl;
+  h.get();
+  cout << endl;
+  k.get();
   cout << endl;
   return 0;
 }
