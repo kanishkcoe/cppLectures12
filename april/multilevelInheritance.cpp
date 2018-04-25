2e063a2c351bece57ebe3467ccca71765673bf52
kanishkcoe <32832273+kanishkcoe@users.noreply.github.com>
2018-04-25 21:09:56 +0530

    250418

---
+++ april/multilevelInheritance.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+using namespace std;
+
+class Person
+{
+  int age;
+  char name[20];
+
+public:
+  void get();
+  void show();
+};
+
+class Student
+{
+  int rollno;
+  float average;
+
+public:
+  void insert();
+  void display();
+};
+
+class GradStudent
+{
+  char subject[20];
+  char working;
+
+public:
+  void enter();
+  void displayDetails();
+};
+
+
+//function definitions
+void Person::get()
+{
+  cin.ignore();
+  cout << "Enter name : ";
+  cin.getline(name, 20);
+
+  cout << "Enter age : ";
+  cin >> age;
+}
+
+void show()
+{
+  cout << "Name : " << name << endl;
+  cout << "Age : " << age << endl;
+}
+
+void Student::insert()
+{
+  cout << "Enter roll number : ";
+  cin >> rollno;
+
+  cout << "Enter average : ";
+  cin >> average;
+}
+
+void Student::display()
+{
+  cout << "Roll number : " << rollno << endl;
+  cout << "Average : " << average << endl;
+}
+
+void GradStudent::enter()
+{
+  cin.ignore();
+  cout << "Enter subject : ";
+  cin.getline(subject, 20);
+
+  cout << "Enter age : ";
+  cin >> age;
+}
+
+void GradStudent::displayDetails()
+{
+  cout << "Subject : " << subject << endl;
+  cout << "Working : " << working << endl;
+}
+
+int main()
+{
+
+
+  return 0;
+}
