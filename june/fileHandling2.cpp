8ae80f15ce7115ac71bbdeb868c3e6760f5731fe
kanishkcoe <32832273+kanishkcoe@users.noreply.github.com>
2018-06-07 20:56:47 +0530

    file handling

---
+++ june/fileHandling2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+  char a[50];
+  int count = 0;
+  // cout << "Enter any string : ";
+  // cin.getline(a, 50);
+
+  //inserted item to the file
+  // ofstream fout;
+  // fout.open("test2.txt", ios::app);
+  // fout << a;
+  // fout.close();
+
+  //getting item from the file
+  ifstream fin;
+  fin.open("test2.txt", ios::in);
+
+  while(!fin.eof())
+  {
+    fin.getline(a, 50);
+    cout << a << endl;
+    count++;
+  }
+  fin.close();
+  cout << count - 2 << endl;
+  return 0;
+}
